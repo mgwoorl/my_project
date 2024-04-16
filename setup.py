5ad246a06b11f8f77f800260ec79d5d5fce5e736
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2024-04-16 10:34:59 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
+from setuptools import setup
 
+setup(
+   name='mtracker',
+   version='1.0',
+   description='Provides a decorator for memory usage tracking. The part of FOSS course.',
+   license='MIT',
+   author='Oksana Galiulina',
+   author_email='oksana17.06@bk.ru',
+   url='https://github.com/standlab/mtracker',
+   packages=['mtracker'], 
+   install_requires=[], # it is empty since we use standard python library
+   extras_require={
+        'test': [
+            'pytest',
+            'coverage',
+        ],
+   },
+   python_requires='>=3',
+)
